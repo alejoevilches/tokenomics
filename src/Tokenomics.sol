0a72f17ddc3ec12731a68fcfc9717f311098e33b
Alejo Ezequiel Vilches <alejoevilches@gmail.com>
2026-01-18 21:06:07 -0300

    start staking feature

--- src/Tokenomics.sol
+++ src/Tokenomics.sol
@@ -3,27 +3,36 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Tokenomics is ERC20 {
-    struct StackPosition {
+    struct StakePosition {
         uint256 amount;
         uint256 lockedUntil;
     }
 
-    mapping(address => StackPosition) public stakedPerAccount;
+    mapping(address => StakePosition) public stakedPerAccount;
     uint256 totalStaked;
     uint256 public constant INITIAL_SUPPLY = 100000 ether;
     uint256 public constant LOCK_PERIOD = 100800; //14 days
     uint256 public volumePerTerm;
+    uint256 public startingTimeOfTerm;
 
     error Stake_InvalidAmount();
     error Unstake_NotEnoughAmountStaked();
     error Unstake_InvalidAmount();
     error Unstake_StakeLocked();
+    error CreateNewSupply_NotEnoughVolume();
 
     event Staked(address account, uint256 amount);
     event Unstaked(address account, uint256 amount);
 
     constructor() ERC20("Tokenomics", "TKN") {
         _mint(msg.sender, INITIAL_SUPPLY);
+    }
+
+    function createNewSupply() public {
+        uint256 total = totalSupply();
+        uint256 adition = (total * 5) / 100;
+        if (volumePerTerm < adition) revert CreateNewSupply_NotEnoughVolume();
+        _mint(address(this), adition);
     }
 
     function stake(uint256 amount) external {
